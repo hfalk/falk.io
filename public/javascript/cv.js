3c2ed2be52ddaf8c6ca4e7d71dcac0230b4829e6
Håvard Falk <haavard@falk.io>
2015-06-09 21:52:18 +0200

    fix navbar in header

--- public/javascript/cv.js
+++ public/javascript/cv.js
@@ -1,1 +1,20 @@
+$(document).ready(function(){
 
+/* affix the navbar after scroll below header */
+$('#nav').affix({
+      offset: {
+        top: $('header').height()
+      }
+});
+
+/* highlight the top nav as scrolling occurs */
+$('body').scrollspy({ target: '#nav' })
+
+/* smooth scrolling for nav sections */
+$('#nav .navbar-nav li>a').click(function(){
+  var link = $(this).attr('href');
+  var posi = $(link).offset().top;
+  $('body,html').animate({scrollTop:posi},700);
+});
+
+});
