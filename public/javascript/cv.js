a75f4eb159642b399884c1ee5bebe1ec89aa9064
Håvard <haavard@falk.io>
2015-08-03 16:39:58 +0200

    fix navbar and edu/exp

--- public/javascript/cv.js
+++ public/javascript/cv.js
@@ -1,20 +1,31 @@
+(function ($) {
+
+    //JQuery sticky for navbar sticky
+    $(window).load(function(){
+        $("#nav").sticky({ topSpacing: 0 });
+    });
+
+    //jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function() {
+        $('.navbar-nav li a').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+        $('.page-scroll a').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
+
+})(jQuery);
+
 $(document).ready(function(){
-
-/* affix the navbar after scroll below header */
-$('#nav').affix({
-      offset: {
-        top: $('header').height()
-      }
-});
-
-/* highlight the top nav as scrolling occurs */
-$('body').scrollspy({ target: '#nav' })
-
-/* smooth scrolling for nav sections */
-$('#nav .navbar-nav li>a').click(function(){
-  var link = $(this).attr('href');
-  var posi = $(link).offset().top;
-  $('body,html').animate({scrollTop:posi},700);
-});
+    $('body').scrollspy({ target: '#nav' });
 
 });
